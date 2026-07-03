1058a146a1eadc990226d1a94bd1d266cc77c1bc
shubham <shubham@DESKTOP-ON3LNKU.cms.co.in>
2026-07-03 17:39:45 +0530

    migrate AI service from ChatModel to ChatClient

---
+++ ai.interview/ai.interview/src/main/java/com/ai_interview_assistant/ai/interview/memory/ChatMemory.java
@@ -0,0 +1,18 @@
+//package com.ai_interview_assistant.ai.interview.memory;
+//
+//import org.springframework.ai.chat.messages.Message;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Component
+//public class ChatMemory {
+//
+//    private final List<Message> messages = new ArrayList<>();
+//
+//    public List<Message> getMessages() {
+//        return messages;
+//    }
+//
+//}
--- ai.interview/ai.interview/src/main/java/com/ai_interview_assistant/ai/interview/service/AIService.java
+++ ai.interview/ai.interview/src/main/java/com/ai_interview_assistant/ai/interview/service/AIService.java
@@ -1,30 +1,36 @@
 package com.ai_interview_assistant.ai.interview.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
+import org.springframework.ai.chat.messages.Message;
 
 import com.ai_interview_assistant.ai.interview.config.PromptConfig;
 
 @Service
 public class AIService {
 
-    private final ChatModel chatModel;
+	private final ChatClient chatClient;
 
-    public AIService(ChatModel chatModel) {
-        this.chatModel = chatModel;
+    public AIService(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     public String askAI(String userMessage) {
-    	
-    	Prompt prompt = new Prompt(
-    			new SystemMessage(PromptConfig.SYSTEM_PROMPT),
-    			new UserMessage(userMessage));
 
-        return chatModel.call(prompt).getResult().getOutput().getText();
+        return chatClient
+                .prompt()
+                .system(PromptConfig.SYSTEM_PROMPT)
+                .user(userMessage)
+                .call()
+                .content();
 
     }
-
 }
